d9d4d00139202c4b8d4e5ff661a2ba4c881ffe96
Ahmed <ahmedalhajheder@gmail.com>
2023-04-15 14:21:26 +0200

    extracted a funtion & refactoring

--- game.js
+++ game.js
@@ -3,69 +3,48 @@ let title=document.querySelector(".title")
 let turn="X"
 let squares=[]
 function detectWinner(){
-    for(let i=1; i<10; i++)
+    for(let i = 1; i < 10; i++)
     {
-        squares[i]=document.getElementById("item" + i).innerHTML
+        squares[i] = document.getElementById("item" + i).innerHTML
     }
-    
-    if(squares[1] === squares[2] && squares[2] === squares[3] && squares[3] != ""){
-        title.innerHTML= `${squares[1]}` + " is th winner";
-        document.getElementById("item" + 1).style.background= "#000";
-        document.getElementById("item" + 2).style.background= "#000";
-        document.getElementById("item" + 3).style.background= "#000";
+
+    checkWinnerForThreeBlocks(1,2,3)
+    checkWinnerForThreeBlocks(4,5,6)
+    checkWinnerForThreeBlocks(7,8,9)
+    checkWinnerForThreeBlocks(1,5,9)
+    checkWinnerForThreeBlocks(3,5,7)
+}
+
+
+function checkWinnerForThreeBlocks(first, second, third){
+    if(squares[first] === squares[second] && squares[second] === squares[third] && squares[third] != ""){
+        title.innerHTML= `${squares[first]}` + " is th winner";
+
+        document.getElementById("item" + first).style.background= "#000";
+        document.getElementById("item" + second).style.background= "#000";
+        document.getElementById("item" + third).style.background= "#000";
+
         setInterval(function(){ title.innerHTML +="."},1000);
-        setTimeout(function(){location.reload()},4000)
-    }
-    else if(squares[4] === squares[5] && squares[5] === squares[6] && squares[6] != ""){
-        title.innerHTML= `${squares[4]}` + " is th winner";
-        document.getElementById("item" + 4).style.background= "#000";
-        document.getElementById("item" + 5).style.background= "#000";
-        document.getElementById("item" + 6).style.background= "#000";
-        setInterval(function(){ title.innerHTML +="."},1000);
-        setTimeout(function(){location.reload()},4000)
-    }
-    else if(squares[7] === squares[8] && squares[8] === squares[9] && squares[9] != ""){
-        title.innerHTML= `${squares[7]}` + " is th winner";
-        document.getElementById("item" + 7).style.background= "#000";
-        document.getElementById("item" + 8).style.background= "#000";
-        document.getElementById("item" + 9).style.background= "#000";
-        setInterval(function(){ title.innerHTML +="."},1000);
-        setTimeout(function(){location.reload()},4000)
-    }
-    else if(squares[1] === squares[5] && squares[5] === squares[9] && squares[9] != "")
-    {
-        title.innerHTML= `${squares[1]}` + " is th winner";
-        document.getElementById("item" + 1).style.background= "#000";
-        document.getElementById("item" + 5).style.background= "#000";
-        document.getElementById("item" + 9).style.background= "#000";
-        setInterval(function(){ title.innerHTML +="."},1000);
-        setTimeout(function(){location.reload()},4000)
-    }
-    else if(squares[3] === squares[5] && squares[5] === squares[7] && squares[7] != ""){
-        title.innerHTML= `${squares[3]}` + " is th winner";
-        document.getElementById("item" + 3).style.background= "#000";
-        document.getElementById("item" + 5).style.background= "#000";
-        document.getElementById("item" + 7).style.background= "#000";
-        setInterval(function(){ title.innerHTML += "."},1000);
         setTimeout(function(){location.reload()},4000)
     }
 }
 
-
 function setSquareContent(id){
     let element= document.getElementById(id);
-    if (turn === "X" && element.innerHTML === "")
-    {
-      element.innerHTML="X" 
-      turn="O" 
-      title.innerHTML="O"
-    }
-    if(turn === "O"  && element.innerHTML === "")
-    {
-        element.innerHTML = "O";
-        turn = "X"
-        title.innerHTML = "X"
-    }
+    if(element.innerHTML === ""){
+        if (turn === "X")
+        {
+          element.innerHTML="X" 
+          turn="O" 
+          title.innerHTML="O"
+        }
+        else if(turn === "O")
+        {
+            element.innerHTML = "O";
+            turn = "X"
+            title.innerHTML = "X"
+        }
 
-    detectWinner();
+        detectWinner();
+    }
 }
